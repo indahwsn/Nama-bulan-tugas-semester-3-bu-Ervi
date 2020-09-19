cfe99439194a77ca62f1c62a60c1dc8c96a50dc8
Indah Widya Surya Ningrat <70737470+indahwsn@users.noreply.github.com>
2020-09-19 21:51:29 +0700

    Create script.java

---
+++ script.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2019;
+
+/**
+ *
+ * @author Indah Widya SN
+ */
+public class IndahWidyaSuryaNingrat {
+    public static void main (String [ ] args) {
+        int nilai = 7 ;
+switch (nilai) {
+case 1:
+System.out.println("Januari");
+case 2:
+System.out.println("Februari");  break;
+case 3:
+System.out.println("Maret");  break;
+case 4:
+System.out.println("April");  break;
+case 5:
+System.out.println("Mei");  break;
+case 6:
+System.out.println("Juni");  break;
+case 7:
+System.out.print("Juli");  break;
+case 8:
+System.out.print("Augustus");  break;
+case 9:
+System.out.print("September");  break;
+case 10:
+System.out.print("Oktober");  break;
+case 11:
+System.out.print("November");  break;
+case 12:
+System.out.print("Desember");  break;
+default:
+System.out.print("Nilai tidak ada");  break;
+}
+}
+
+    
+}
